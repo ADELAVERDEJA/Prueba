930117bff99297ec239262c7040bacdb06883f83
ADELAVERDEJA <adelaverdeja@gmail.com>
2017-10-18 15:29:26 +0200

    Update Prueba.java

--- Prueba/src/prueba/Prueba.java
+++ Prueba/src/prueba/Prueba.java
@@ -18,6 +18,8 @@ public class Prueba {
      */
     public static void main(String[] args) {
         JOptionPane.showMessageDialog(null,"hola");
+        JOptionPane.showMessageDialog(null,"hola");
+        JOptionPane.showMessageDialog(null,"hola");
     }
     
 }
